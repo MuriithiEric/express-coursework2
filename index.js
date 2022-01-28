9e068f22599c6a53f6fb3ab237fb68f70fb2afea
Favour Felix <felixfavour0@gmail.com>
2022-01-28 20:07:37 +0400

    made changes to lessons and index.js

--- index.js
+++ index.js
@@ -5,48 +5,21 @@ var http = require("http");
 var app = express();
 const port = 3000;
 
-//var lessons = require("./data/lessons.json");
-//var users = require("./data/users.json");
+var lessons = require("./data/lessons.json");
+var user = require("./data/user.json");
 
 //CORS allows you to configure the web API's security. It has to do with allowing other domains to make requests against your web API.
 app.use(cors());
 
-const courses = [
-  { topic: "math", location: "London", price: 100 },
-  { topic: "math", location: "Liverpool", price: 80 },
-  { topic: "math", location: "Oxford", price: 90 },
-  { topic: "math", location: "Bristol", price: 120 },
-];
-
-http
-  .createServer((req, res) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(JSON.stringify(courses));
-  })
-  .listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
+app
+.listen(port, () => {
+console.log(`Server running at http://localhost:${port}`);
+});
 
 app.get("/lessons", (req, res) => {
-  res.send([
-    { topic: "math", location: "London", price: 100 },
-    { topic: "math", location: "Liverpool", price: 80 },
-    { topic: "math", location: "Oxford", price: 90 },
-    { topic: "math", location: "Bristol", price: 120 },
-  ]);
+  res.status(200).json(lessons); 
 });
 
-app.get("/users", (req, res) => {
-  res.send([
-    {
-      email: "user@email.com",
-      password: "mypassword",
-    },
-  ]);
+app.get("/user", (req, res) => {
+  res.status(200).json(user); 
 });
-
-//starts the server on port 3000
-//app.listen(3000);
-
-//console.log(`App is listening on port ${3000}`);
